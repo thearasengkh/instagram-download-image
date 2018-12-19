75f580f760a96f09382502ddaee93e985ddb4947
Theara Seng <seng.theara.kh@gmail.com>
2018-12-19 23:01:42 +0700

    add download script

---
+++ download.js
@@ -0,0 +1,22 @@
+function downloadImage() {
+
+	var link = document.createElement("a");
+
+	link.setAttribute("href", $('body > div:nth-child(15) > div > div.zZYga > div > article > div._97aPb > div > div > div.KL4Bh > img').src);
+	link.setAttribute("download", "");
+	link.setAttribute("target", "_blank");
+	link.click();
+
+}
+
+
+function downloadImageByIndex(index) {
+
+	var link = document.createElement("a");
+
+	link.setAttribute("href", $('body > div:nth-child(15) > div > div.zZYga > div > article > div._97aPb > div > div > div > div.tN4sQ.zRsZI > div > div.MreMs > div > ul > li:nth-child('+index+') > div > div > div > div.KL4Bh > img').src);
+	link.setAttribute("download", "");
+	link.setAttribute("target", "_blank");
+	link.click();
+
+}
