97298939d32621aaac1ebc7a592abde3404beb5f
Theara <seng.theara.kh@gmail.com>
2019-02-02 22:07:43 +0700

    update downloadImageByIndex()

--- download.js
+++ download.js
@@ -14,7 +14,7 @@ function downloadImageByIndex(index) {
 
 	var link = document.createElement("a");
 
-	link.setAttribute("href", $('body > div:nth-child(15) > div > div.zZYga > div > article > div._97aPb > div > div > div > div.tN4sQ.zRsZI > div > div.MreMs > div > ul > li:nth-child('+index+') > div > div > div > div.KL4Bh > img').src);
+	link.setAttribute("href", $('body > div:nth-child(14) > div > div.zZYga > div > article > div._97aPb > div > div > div > div.tN4sQ.zRsZI > div > div > div > ul > li:nth-child('+index+') > div > div > div > div.KL4Bh > img').src);
 	link.setAttribute("download", "");
 	link.setAttribute("target", "_blank");
 	link.click();
